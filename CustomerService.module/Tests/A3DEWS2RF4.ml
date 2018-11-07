88ce01c85b3aced63fdf0640025d9ce1e62ee7a4
sukhdevsingh <sukhdevsingh@10.95.219.46>
2018-11-07 18:48:31 -0500

    customer service repo

---
+++ CustomerService.module/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/CustomerService.module/Tests/CustomerDetailsTest.bwt#/"/>
+  <EmulationDataFile>ACED0005740017437573746F6D657244657461696C73546573742E627774</EmulationDataFile>
+</emulation:BWTFile>
